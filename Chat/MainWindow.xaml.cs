b321eab1b91df3bec84f4441df2f5fc51d54dc2d
David Stopka <ironmansbr09@gmail.com>
2023-08-03 07:10:47 -0500

    Committing latest changes.

--- Chat/MainWindow.xaml.cs
+++ Chat/MainWindow.xaml.cs
@@ -57,13 +57,16 @@ namespace Chat
 
         public static Socket client = null;
 
-        public tcpClass tcpInstance;
+
+
+
+        public tcpClass tcpInstance = null;
 
 
         //  the following are to run send_message() and receive_message() at the same time, for bi-directional messaging
-        System.Threading.Thread t1;
+        public static System.Threading.Thread t1;
 
-        System.Threading.Thread t2;
+        public System.Threading.Thread t2;
 
 
         //  this is the intitial message in TextBox
@@ -84,6 +87,13 @@ namespace Chat
             TextBox_Msg = TextBox_enterMessage;
 
             RTB_local = RichTextBox_Messages;
+
+
+            //  DispatcherTimer setup
+            DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = TimeSpan.FromSeconds(5);
+            dispatcherTimer.Start();
 
 
         }
@@ -118,6 +128,8 @@ namespace Chat
                 tcpInstance.initiate_connection_tcp_server();
 
             }
+
+
 
         }
 
@@ -212,6 +224,8 @@ namespace Chat
 
                 Console.WriteLine("client_ip: " + client_ip + " client_port: " + client_port);
 
+                
+
 
             }
 
@@ -221,57 +235,89 @@ namespace Chat
 
 
             //  methods
-            public async void initiate_connection_tcp_server()
+            //public async void initiate_connection_tcp_server()
+            public void initiate_connection_tcp_server()
             {
 
-
-                //Application.Current.Dispatcher.Invoke(() => {
-
-                    //IP_and_Port = IP_and_port_string.Text;
-                    //IP_Port_columns = IP_and_Port.Split(',');
-                    
-                //});
-
-
-                //IPAddress localAddr = IPAddress.Parse("192.168.0.54");
-                //localAddr = IPAddress.Parse("192.168.0.54");  //  localhost
-                //localAddr = IPAddress.Parse("69.246.226.60");  // public IP
-                //localAddr = IPAddress.Parse(IP_Port_columns[0] );  // public IP
-
-                //server = new TcpListener( localAddr, Convert.ToInt32(client_port));
-
-                //server = new TcpListener(IPAddress.Parse(client_ip), Convert.ToInt32(client_port));
-
                 server = new TcpListener( IPAddress.Parse(client_ip) , Convert.ToInt32(client_port) );
-
 
                 server.Start();
                 
                 bytes = new byte[256];
 
-                Console.WriteLine(" Waiting for a connection.");
+                Console.WriteLine(" Waiting for a connection (blocking call to server.AcceptTcpClient().");
 
                 //  Perform a blocking call to accept requests.
                 //  You can also use server. AcceptSocket() here.
-                //tcpClient client = server.AcceptTcpClient();
                 client = server.AcceptTcpClient();
 
-
-                //TB_local.Text = "Initializing...";
-
-                Console.WriteLine("socket Is Connected!");
+                Console.WriteLine(Environment.NewLine + " socket Is Connected!");
 
                 StatusButton.Background = Brushes.Green;
+
                 StatusButton.Content = "Connected!";
 
-                Console.WriteLine("StatusButton should have updated to GREEN by now.");
+                Console.WriteLine( Environment.NewLine + " StatusButton should have updated to GREEN by now.");
 
                 String data = null;
 
 
 
+            //    t1 = new Thread( runDispatcherTimer );
+
+            //    t1.Start();
+
                 return;
             }
+
+
+            //public void runDispatcherTimer()
+            //{
+
+                
+                
+
+            //}
+
+            public bool checkConnection() { 
+
+
+                bool isConnected = true;
+
+
+                //for (;;) {
+
+                    if ( client.Connected )
+                    {
+
+                        Console.WriteLine(" Client IS connected.");
+
+                        return true;
+
+
+                    }
+                    else if (! client.Connected)
+                    {
+
+                        Console.WriteLine(" Client is NOT connected.");
+
+                        return false;
+
+                    }
+
+
+                //}
+
+
+                return false;
+            }
+
+
+
+
+
+
+
 
 
 
@@ -279,11 +325,6 @@ namespace Chat
             public bool initiate_connection_tcp_client()
             {
 
-                //System.Net.IPAddress ip = System.Net.IPAddress.Parse(client_ip);
-
-                //System.Net.IPEndPoint ipEndPoint = new System.Net.IPEndPoint( System.Net.IPAddress.Parse(client_ip), Convert.ToInt32(client_port));
-
-                //client = new TcpClient( client_ip, Convert.ToInt32(client_port) );
                 client = new TcpClient( client_ip, Convert.ToInt32(client_port));
 
                 StatusButton.Background = Brushes.Green;
@@ -348,7 +389,7 @@ namespace Chat
 
                     if (this.role == "server")
                     {
-                        myParagraph.Foreground = Brushes.LightGreen;
+                        myParagraph.Foreground = Brushes.Green;
 
                     }
 
@@ -395,12 +436,12 @@ namespace Chat
                     //  Translate data bytes to ASCII string.
                     data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
                     data = data.TrimEnd(new char[] { '\r', '\n' });
-                    data = " Received: " + data;
+                    data = " Recv'd msg: " + data;
                     data = data.TrimEnd(new char[] { 'r', '\n' });
                     Console.WriteLine( data);
 
                     //  Process the data sent by the client.
-                    data = data.ToUpper();
+                    //data = data.ToUpper();
 
                     byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
 
@@ -423,7 +464,7 @@ namespace Chat
 
                         if (this.role == "client")
                         {
-                            myParagraph.Foreground = Brushes.LightGreen;
+                            myParagraph.Foreground = Brushes.Green;
                         }
 
 
@@ -551,8 +592,32 @@ namespace Chat
 
 
 
+        //  EVENT HANDLERS
 
 
+
+
+        //  the following to check the state of the tcp unicast connection every five seconds
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+
+            Console.WriteLine(Environment.NewLine + DateTime.Now + " Firing dispatcherTimer");
+
+            bool result = false;
+
+            if (tcpInstance != null) {
+
+                result = tcpInstance.checkConnection();
+
+            }
+            Console.WriteLine(Environment.NewLine + DateTime.Now + " tcp Connection Status: " + result);
+
+
+
+
+            // Forcing the CommandManager to raise the RequerySuggested event
+            CommandManager.InvalidateRequerySuggested();
+        }
 
 
 
@@ -563,6 +628,11 @@ namespace Chat
             Console.WriteLine(" launching App");
 
             initiate_tcp();
+
+
+
+
+
 
         }
 
@@ -640,6 +710,10 @@ namespace Chat
             t2 = new Thread(this.tcpInstance.receive_message);
 
             t2.Start();
+
+
+
+
 
 
         }
